e1e62c5048d8a0e549dd22ee88224f95d884387b
alaa <alaashawa1995@gmail.com>
2023-02-11 13:27:21 +0200

    livewire component

---
+++ app/Http/Livewire/Delay.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Delay extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+              <h1>alaa shawa</h1>
+            </div>
+        blade;
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+|<button  type="button" class="btn btn-primary" wire:click="Increment">+</button>
+
+ <h1>{{$count}}</h1>
+ |<button  type="button" class="btn btn-primary"wire:click="Decrement">_</button>
+
+</div>
